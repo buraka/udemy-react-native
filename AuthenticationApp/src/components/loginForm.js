2abc03f1e055bf0eb21a03fc6be80a097cded00a
Burak Alparslan <brk.alprsln@gmail.com>
2018-10-08 23:14:47 +0300

    Added spinner mech for authentication app

--- AuthenticationApp/src/components/loginForm.js
+++ AuthenticationApp/src/components/loginForm.js
@@ -1,40 +1,62 @@
 import React, { Component } from 'react';
 import { View, Button, Text, StyleSheet } from 'react-native';
 import firebase from 'firebase';
-import { Input } from './common';
+import { Input, Spinner } from './common';
 
 class LoginForm extends Component {
   state = {
     email: '',
     password: '',
-    error: ''
+    error: '',
+    loading: false
   }
 
   onButtonClicked() {
     const { email, password } = this.state;
     this.setState({
-      error: ''
+      error: '',
+      loading: true
     });
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .catch((err) => {
+      .then(this.onLoginSuccess.bind(this))
+      .catch(() => {
         firebase.auth().createUserWithEmailAndPassword(email, password)
-          .catch((error) => {
-            console.log('hata'); //TODO
-            this.setState({
-              error: 'Authentication failed'
-            });
-          });
+          .then(this.onLoginSuccess.bind(this))
+          .catch(this.onLoginFailed.bind(this));
       });
   }
 
+  onLoginSuccess() {
+    this.setState({
+      email: '',
+      password: '',
+      error: '',
+      loading: false
+    });
+  }
+
+  onLoginFailed() {
+    this.setState({
+      error: 'Authentication failed',
+      loading: false
+    });
+  }
   render() {
-    const { error } = this.state;
+    const { error, loading } = this.state;
 
     const errorMsg = error ? (
       <Text style={styles.errorText}>
         {error}
       </Text>
-    ) : null
+    ) : null;
+
+    const loginButton = loading ? (
+      <Spinner />
+    ) : (
+      <Button onPress={this.onButtonClicked.bind(this)}
+              color='#E87B79'
+              title='Login'/>
+    )
     return (
       <View style={{ padding: 30 }}>
         <View>
@@ -58,7 +80,7 @@ class LoginForm extends Component {
         </View>
         {errorMsg}
         <View style={styles.buttonWrapper}>
-          <Button onPress={this.onButtonClicked.bind(this)} color='#E87B79' title='Login'/>
+          {loginButton}
         </View>
       </View>
     )
